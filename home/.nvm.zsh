3c3884e5841dae1906bdf5d5e86398612afa08bd
Jeffrey Horn <jeff@jrhorn.me>
2017-06-14 14:44:42 -0400

    Source the correct nvm

--- home/.nvm.zsh
+++ home/.nvm.zsh
@@ -1,5 +1,5 @@
 export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh" # This loads nvm
+. "/usr/local/opt/nvm/nvm.sh"
 
 # Auto change the nvm version based on a .nvmrc file based on the current directory. See https://github.com/creationix/nvm/issues/110#issuecomment-190125863
 autoload -U add-zsh-hook
